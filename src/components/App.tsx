2033e460a8752406c525533b3aad1d69ab39bd99
marco-vidali <marco-vidali-3@proton.me>
2023-10-15 13:58:07 +0200

    Added small screen error

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,8 @@
-import { useState } from "react";
-import Data from "./Data";
+import { useState, useEffect } from "react";
 import Simulation from "./Simulation";
-import StartStop from "./StartStop";
 import InputForm from "./InputForm";
+import Data from "./Data";
+import StartStop from "./StartStop";
 
 export default function App() {
   const [started, setStarted] = useState(false);
@@ -10,6 +10,23 @@ export default function App() {
   const [s0Y, setS0Y] = useState<number | null>(null);
   const [v0, setV0] = useState<number | null>(null);
   const [angle, setAngle] = useState<number | null>(null);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  // Add an event listener to update the window width state
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  // Define the minimum width at which the message should appear
+  const minWindowWidth = 1100;
 
   function handleStarted() {
     if (started) {
@@ -27,33 +44,43 @@ export default function App() {
 
   return (
     <>
-      {started ? (
-        <Simulation s0X={s0X!} s0Y={s0Y!} v0={v0!} angle={angle!} />
+      {windowWidth < minWindowWidth ? (
+        <div className="flex h-screen items-center justify-center">
+          <p className="text-center max-w-[90vw]">
+            This app is unusable on small screens
+          </p>
+        </div>
       ) : (
-        <Data>
-          <InputForm
-            placeholder="Initial X position (m)"
-            value={s0X}
-            onChange={setS0X}
-          />
-          <InputForm
-            placeholder="Initial Y position (m)"
-            value={s0Y}
-            onChange={setS0Y}
-          />
-          <InputForm
-            placeholder="Initial velocity module (m/s)"
-            value={v0}
-            onChange={setV0}
-          />
-          <InputForm
-            placeholder="Initial velocity angle"
-            value={angle}
-            onChange={setAngle}
-          />
-        </Data>
+        <>
+          {started ? (
+            <Simulation s0X={s0X!} s0Y={s0Y!} v0={v0!} angle={angle!} />
+          ) : (
+            <Data>
+              <InputForm
+                placeholder="Initial X position (m)"
+                value={s0X}
+                onChange={setS0X}
+              />
+              <InputForm
+                placeholder="Initial Y position (m)"
+                value={s0Y}
+                onChange={setS0Y}
+              />
+              <InputForm
+                placeholder="Initial velocity module (m/s)"
+                value={v0}
+                onChange={setV0}
+              />
+              <InputForm
+                placeholder="Initial velocity angle"
+                value={angle}
+                onChange={setAngle}
+              />
+            </Data>
+          )}
+          <StartStop started={started} onToggleStarted={handleStarted} />
+        </>
       )}
-      <StartStop started={started} onToggleStarted={handleStarted} />
     </>
   );
 }
